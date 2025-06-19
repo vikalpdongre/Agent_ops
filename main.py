3c404e826ee911d14913af95487664a411d0c468
vikalpdongre <35396484+vikalpdongre@users.noreply.github.com>
2025-06-19 19:26:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,5 +17,5 @@ def analyze_resume():
 
 #if __name__ == '__main__':
 #    app.run(host='0.0.0.0', port=8080)
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
